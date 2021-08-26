02a927849a1a2b03543cef0d1a8164456983fd34
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-08-26 17:05:13 -0300

    ejercicios-1

---
+++ script.js
@@ -0,0 +1,4 @@
+// Detener el envio del formulario (cualquiera de las formas) al hacer clic en el boton 'Enviar' e imprimir por consola algun mensaje cada vez que se haga clic en el mismo.
+// Capturar e imprimir por consola los datos que la persona coloque en el formulario.
+// Pintar en el HTML (en cualquier elemento creado para tal fin) algun dato (o todos) de la informacion del formulario al hacer clic en el boton 'JS'. Se puede crear este elemento (o elementos) tanto desde el HTML como utilizando JS como se vio en clases.
+// Agregar en el HTML un boton en el header cuyo unico proposito sea cambiar el color del mismo y realizar la logica para que pueda funcionar como tal (utilizar ID en el header para seleccionarlo y trabajar con el DOM y la propiedad classList de los elementos HTML como se vio en clases. Investigar sobre los metodos 'toggle', 'add', y 'remove' para trabajar con clases de CSS desde JS).
